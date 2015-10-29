30c42f07593a9065b683fa301fc63f862d23dd28
椛島優 <icloud_i4s@me.com>
2015-10-29 12:36:46 +0900

    Initial Commit

---
+++ eslpods/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  eslpods
+//
+//  Created by 椛島優 on 2015/10/29.
+//  Copyright © 2015年 椛島優. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ eslpodsTests/eslpodsTests.m
@@ -0,0 +1,39 @@
+//
+//  eslpodsTests.m
+//  eslpodsTests
+//
+//  Created by 椛島優 on 2015/10/29.
+//  Copyright © 2015年 椛島優. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface eslpodsTests : XCTestCase
+
+@end
+
+@implementation eslpodsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
